5984e9b2cc9989bcee5ddce6cd3291ac48f7f47f
Alex Aulbach <alex.aulbach@mayflower.de>
2014-05-23 15:27:42 +0200

    Added translations-class and made it running properly.

--- Xml2Wiki.php
+++ Xml2Wiki.php
@@ -1,28 +1,58 @@
 #!/usr/bin/php
 <?php
 /**
+ * Convert generated XML-file (by LuaRaw2XML.lua) to Wiki-pages
+ *
+ * needs as first parameter the xml-file. Example:
+ * > ./Xml2Wiki.php tmp/factorio-data.xml
+ *
+ *
  * export XDEBUG_CONFIG="idekey=PHPSTORM"
  */
 
 ini_set('include_path', ini_get('include_path') . ':' . dirname(__FILE__) . DIRECTORY_SEPARATOR . "lib");
 include_once('FactContent.php');
 include_once('FactPage.php');
+include_once('FactTrans.php');
 
 $xml = simplexml_load_file($argv[1]);
 $content = new FactContent($xml);
 
+// debug
 print_r($content->tree['raw']['item']);
-
 print_r($content->getItemKeysSorted());
 print_r($content->getItemKeys());
 
 
+createItemPages($content);
 
+/**
+ * create ITEM pages
+ *
+ * @param FactContent $content
+ */
 function createItemPages(FactContent $content) {
     foreach ($content->getItemKeys() as $key) {
-
+        echo "Page: $key\n";
 
     }
 }
+
+
+// create item-overview pages
+
+// create item-templates
+
+
+// ....
+
+// create ENTITY-pages
+
+
+// create RECIPE-pages
+
+
+// create RESEARCH-pages
+
 
 
--- lib/FactTrans.php
+++ lib/FactTrans.php
@@ -4,17 +4,78 @@
 
 class FactTrans {
 
-    protected $_trans = array();
+    protected $_translationCache = array();
 
-    public function __construct($path)
+    protected $_availableLanguages = array();
+
+    protected $_inifilesPerLanguage = array();
+
+    public function __construct(array $translationBasedirs)
     {
-        $dirs = opendir($path);
+        foreach ($translationBasedirs as $dir) {
+            // construct new
+            $languagesDirHandle = dir($dir);
 
-
-        foreach ($dirs as $lang) {
-            $translations = parse_ini_file($path . DIRECTORY_SEPARATOR . $lang, true);
-            $this->_trans[$lang] = $translations;
+            while (false !== ($language = $languagesDirHandle->read())) {
+                if (!in_array($language, $this->_availableLanguages)) {
+                    $this->_availableLanguages[] = $language;
+                }
+                $inifilesDirHandle = dir($translationBasedirs . DIRECTORY_SEPARATOR . $language);
+                while (false !== ($inifile = $inifilesDirHandle->read())) {
+                    $ini = preg_replace('/.ini$/', '', $inifile);
+                    // not handling multiple ini-files per language yet!
+                    $this->_inifilesPerLanguage[$language][$ini] = $translationBasedirs . DIRECTORY_SEPARATOR . $language . DIRECTORY_SEPARATOR . $inifile;
+                }
+            }
         }
+    }
+
+    public function getAvailableLanguages()
+    {
+        return $this->_availableLanguages;
+    }
+
+    public function getTranslation($lang, $ini, $needle)
+    {
+        $translations = $this->_getTranslationsFromCache($lang, $ini);
+
+        if (is_null($translations)) {
+            $translations = $this->_readTranslations($lang, $ini);
+            $this->_setTranslationsCache($lang, $ini, $translations);
+        }
+
+        if (array_key_exists($needle, $translations)) {
+            return $translations[$needle];
+        }
+
+        return null;
+    }
+
+
+    protected function _readTranslations($lang, $ini)
+    {
+        if (empty($this->_inifilesPerLanguage[$lang][$ini])) {
+            throw new InvalidArgumentException("No ini-file for lang: '$lang', ini: '$ini'");
+        }
+
+        $translationsPerIni = parse_ini_file($this->_inifilesPerLanguage[$lang][$ini], true);
+        return $translationsPerIni;
+    }
+
+    protected function _getTranslationsFromCache($lang, $ini)
+    {
+        if (array_key_exists($ini, $this->_translationCache[$lang])) {
+            return $this->_translationCache[$lang][$ini];
+        }
+        return null;
+    }
+
+    protected function _setTranslationsCache($lang, $ini, $translations)
+    {
+        if (array_key_exists($ini, $this->_translationCache[$lang])) {
+            throw new InvalidArgumentException("Already filled cache for lang: '$lang', ini: '$ini'");
+        }
+        $this->_translationCache[$lang][$ini] = $translations;
     }
 
 
